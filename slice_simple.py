21be6e3e5ce4904cba9f224f68dc475c5d2af7a6
Mariano-Faraldo <mfaraldo@mail.austral.edu.ar>
2025-08-29 14:02:15 -0300

    Add files via upload

--- slice_simple.py
+++ slice_simple.py
@@ -1,5 +1,7 @@
 def slice_simple():
     texto = "Awesome"
+    # Código a implementar, se debe utilizar la variable 'texto' para resolver el ejercicio.
+    # No se debe modificar la definición de la función, ni ingresar otro valor mediante input.
     primeras_tres_letras = texto[:3].lower()
     medio_siendo_tres = texto[2:5].lower()
     Todo_lower = texto[:].lower()
@@ -8,5 +10,8 @@ def slice_simple():
     print(medio_siendo_tres)
     print(Todo_lower)
 slice_simple()
+
+# Para verificar este ejercicio ejecutar el comando
+# `pytest tp3_slice_simple_test.py` o `python tp3_slice_simple_test.py`
 
 # Mariano Faraldo
