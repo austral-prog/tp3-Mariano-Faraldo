620891297b2a3d7307795d7e219ad2bde614d0be
Mariano-Faraldo <mfaraldo@mail.austral.edu.ar>
2025-08-29 13:58:00 -0300

    Add files via upload

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,5 +1,10 @@
 def slice_advanced():
-    # Código a implementar utilizando input.
+    texto = input()
+    resultado = texto[4::2]
+    print(resultado)
 
-# Para verificar este ejercicio ejecutar el comando
-# `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
+slice_advanced()
+
+# mensaje a imprimir: "Hello, World!"
+
+# Mariano Faraldo
